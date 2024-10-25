ec059ef9a772e15571b12ddb593c231bf2783222
tdog <161662619+tdogpog@users.noreply.github.com>
2024-10-24 20:33:55 -0500

    deleted vite defaults

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Header from "./components/childcomponents/Header";
+import MainParent from "./components/MainParent";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Header />
+    <MainParent />
+  </StrictMode>
+);
